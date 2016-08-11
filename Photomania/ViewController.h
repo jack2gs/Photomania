c67c816db44663df365767ac0f246d4ba4fc9d1a
jack2gs <sailor.g@163.com>
2016-08-11 20:31:57 +0800

    Initial Commit

---
+++ Photomania/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photomania
+//
+//  Created by Gao Song on 8/11/16.
+//  Copyright © 2016 Gao Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
